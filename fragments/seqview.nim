897c2a8819c5bc76fc54adeca15add488928c31c
Giovanni <sinkingsugar@gmail.com>
2018-08-09 13:08:15 +0900

    improve by using ptr to element rather then to seq, added write operator

--- fragments/seqview.nim
+++ fragments/seqview.nim
@@ -1,17 +1,29 @@
 type
   SeqView*[T] = object
-    root: ptr seq[T]
-    slice: Slice[int]
+    root: ptr T
+    length: int
 
 proc view*[T](s: var seq[T]; slice: Slice[int]): SeqView[T] {.inline.} =
-  result.root = addr s
-  result.slice = slice
+  result.root = addr s[slice.a]
+  result.length = slice.b - slice.a + 1
 
-proc high*(v: var SeqView): int {.inline.} = v.slice.b - v.slice.a
+proc high*(v: SeqView): int {.inline.} = v.length - 1
 
-proc len*(v: var SeqView): int {.inline.} = v.high + 1
+proc len*(v: SeqView): int {.inline.} = v.length
 
-proc `[]`*[T](v: var SeqView[T]; index: int): T {.inline.} = v.root[][v.slice.a + index]
+proc `[]`*[T](v: SeqView[T]; index: int): T {.inline.} =
+  let
+    buffer = cast[int](v.root)
+    begin = buffer + index * sizeof(T)
+    resPtr = cast[ptr T](begin)
+  return resPtr[]
+
+proc `[]=`*[T](v: SeqView[T]; index: int; value: T) =
+  let
+    buffer = cast[int](v.root)
+    begin = buffer + index * sizeof(T)
+    valPtr = cast[ptr T](begin)
+  valPtr[] = value
 
 when isMainModule:
   var s = @[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -21,3 +33,6 @@ when isMainModule:
   assert(v.len == 4, $v.len)
   assert(v[0] == 2)
   assert(v[v.high] == 5)
+  v[2] = 11
+  assert(v[2] == 11)
+  assert(s[4] == 11)
