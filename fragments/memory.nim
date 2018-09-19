9982e45278edf91961cabda4cf59cab4c9d86871
Jörg Wollenschläger <joerg.wollenschlaeger@gmail.com>
2018-09-19 19:03:37 +0900

    Expose C++ shared pointers

--- fragments/memory.nim
+++ fragments/memory.nim
@@ -14,3 +14,21 @@ proc delete*[T](p: ptr T) {.inline.} =
   when compiles(`=destroy`(p[])):
     `=destroy`(p[])
   dealloc(p)
+
+type
+  SharedPtr* {.importcpp: "std::shared_ptr<'0>", header: "<memory>".} [T] = object
+
+template newShared*(T: typedesc; destructor: untyped): SharedPtr[T] =
+  proc deleter(self: ptr T) =
+    destructor(self[])
+    dealloc(self)
+    
+  proc construct(self: ptr T, del: pointer): SharedPtr[T] {.importcpp: "'0(@)".}
+  let p = newptr(T)
+  construct(p, deleter)
+
+when isMainModule:
+  type Foo = object
+  proc kill(self: var Foo) {.nimcall.} = echo "bye"
+  var x = newShared(Foo, kill)
+  
