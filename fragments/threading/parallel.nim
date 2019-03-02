9bddb21bf049817493412ee3665dcba4df75e97f
Jörg Wollenschläger <joerg.wollenschlaeger@gmail.com>
2019-03-02 19:35:21 +0900

    Use new atomics

--- fragments/threading/parallel.nim
+++ fragments/threading/parallel.nim
@@ -1,13 +1,12 @@
-import cpuinfo, locks, algorithm
+import cpuinfo, locks, algorithm, concurrency/atomics
 import threadpool, threading_primitives
 
 let MaxDegreeOfParallelism = countProcessors()
 
 type
   State = object
-    startInclusive: int
-    activeWorkerCount: int
-    referenceCount: int
+    startInclusive: Atomic[int]
+    activeWorkerCount: Atomic[int]
     finished: ManualResetEvent
 
   SortRange = tuple
@@ -16,8 +15,7 @@ type
 
   SortState = object
     partitions: seq[SortRange]
-    activeWorkerCount: int
-    referenceCount: int
+    activeWorkerCount: Atomic[int]
     finished: ManualResetEvent
 
 proc executeBatch(fromInclusive, toExclusive: int; action: proc(value: int)) =
@@ -31,28 +29,29 @@ proc executeBatch(fromInclusive, toExclusive: int; action: proc(value: int)) =
 proc fork(endExclusive, batchSize, maxDegreeOfParallelism: int; action: proc(value: int), state: ptr State) =
 
   # Other threads already processed all work before this one started. activeWorkerCount is already 0
-  if state.startInclusive >= endExclusive:
+  if state.startInclusive.load(moRelaxed) >= endExclusive:
     return
 
   #  This thread is now actively processing work items, meaning there might be work in progress
-  atomicInc(state.activeWorkerCount)
+  discard state.activeWorkerCount.fetchAdd(1, moRelaxed)
 
   # Kick off another worker if there's any work left
-  if maxDegreeOfParallelism > 1 and state.startInclusive + batchSize < endExclusive:
+  if maxDegreeOfParallelism > 1 and state.startInclusive.load(moRelaxed) + batchSize < endExclusive:
     queueWorkItem do ():
       fork(endExclusive, batchSize, maxDegreeOfParallelism - 1, action, state)
 
   try:
     # Process batches synchronously as long as there are any
-    var newStart = atomicInc(state.startInclusive, batchSize)
+    # TODO: Move this before the queueWorkItem?
+    var start = state.startInclusive.fetchAdd(batchSize, moRelaxed)
 
-    while newStart - batchSize < endExclusive:
-      executeBatch(newStart - batchSize, min(endExclusive, newStart), action)
-      newStart = atomicInc(state.startInclusive, batchSize)
+    while start < endExclusive:
+      executeBatch(start, min(endExclusive, start + batchSize), action)
+      start = state.startInclusive.fetchAdd(batchSize, moRelaxed)
 
   finally:
     # If this was the last batch, signal
-    if (atomicDec(state.activeWorkerCount) == 0):
+    if (state.activeWorkerCount.fetchSub(1, moRelaxed) == 1):
       state.finished.signal()
 
 
@@ -62,8 +61,7 @@ proc parallelFor(fromInclusive, toExclusive: int; action: proc(value: int)) =
   let batchCount = min(MaxDegreeOfParallelism, count)
 
   var state: State
-  state.startInclusive = fromInclusive
-  state.referenceCount = 1
+  state.startInclusive.store(fromInclusive, moRelaxed)
 
   if MaxDegreeOfParallelism <= 1 or batchCount == 1:
     executeBatch(fromInclusive, toExclusive, action)
@@ -72,11 +70,10 @@ proc parallelFor(fromInclusive, toExclusive: int; action: proc(value: int)) =
     let batchSize = (count + (batchCount - 1)) div batchCount
 
     # Kick off a worker, then perform work synchronously
-    atomicInc(state.referenceCount)
     fork(toExclusive, batchSize, MaxDegreeOfParallelism, action, addr(state))
 
     # Wait for all workers to finish
-    if state.activeWorkerCount != 0:
+    if state.activeWorkerCount.load(moRelaxed) != 0:
       state.finished.waitOne()
 
 proc parallelFor[T](collection: openArray[T]; action: proc(item: T)) {.inline.} =
@@ -146,17 +143,16 @@ proc parallelSort[T](collection: var openArray[T]; maxDegreeOfParallelism: int, 
 
         # Add a new worker if necessary
         if maxDegreeOfParallelism > 1 and not hasChild:
-          atomicInc(state.referenceCount)
           queueWorkItem(proc() = parallelSort(collection, maxDegreeOfParallelism - 1, state))
           hasChild = true
 
   finally:
     # If this was the last batch, signal
-    if (atomicDec(state.activeWorkerCount) == 0):
+    if (state.activeWorkerCount.fetchSub(1, moRelaxed) == 1):
       state.finished.set()
 
   # This thread is now actively processing work items, meaning there might be work in progress
-  atomicInc(state.activeWorkerCount)
+  state.activeWorkerCount.fetchAdd(1, moRelaxed)
 
 proc parallelSort[T](collection: var openArray[T]) =
 
@@ -164,17 +160,15 @@ proc parallelSort[T](collection: var openArray[T]) =
     return
 
   var state: SortState
-  state.referenceCount = 1
 
   # Initial partition
   state.partitions.enqueue((0, collection.len - 1))
 
   # Sort recursively
-  inc(state.referenceCount);
   parallelSort(collection, MaxDegreeOfParallelism, state)
 
   # Wait for all work to finish
-  if (state.activeWorkerCount != 0):
+  if (state.activeWorkerCount.load(moRelaxed) != 0):
     state.finished.waitOne()
 
 when isMainModule:
@@ -182,5 +176,10 @@ when isMainModule:
   import os
   import times
 
+  let startTime = epochTime()
+  
   parallelFor(0, 100) do (i: int) -> void:
+    sleep(10)
     echo $i, " ", $getThreadId()
+
+  echo epochTime() - startTime
